204c75771ae6f5ed405bfe9f423ad5b5c93a6d6b
voidrobyn <92083124+voidrobyn@users.noreply.github.com>
2022-11-12 16:20:49 +0000

    AI moves
    AI can move randomly correctly.

--- TryNotToDieInTheMaze/Assets/Scripts/AI Scripts/AIMovement.cs
+++ TryNotToDieInTheMaze/Assets/Scripts/AI Scripts/AIMovement.cs
@@ -8,24 +8,23 @@ public class AIMovement : MonoBehaviour
 {
     private Vector3 m_targetLocation;
     [SerializeField] GameObject player;
-    private bool m_playerInRange;
-    private AIState m_state;
-    public float MaxDistanceToChase;
+    private AIState m_state = AIState.WANDER;
+    public float MaxDistanceToChase = 50f;
     [SerializeField] float WanderSpeed; // the speed you go when in wander
     [SerializeField] float ChaseSpeed; //the speed you go when in chase
     [SerializeField] GameObject mazeGen;
     private int[,] maze;
     private NavMeshPath m_CurrentPath;
-    private NavMeshAgent m_Agent;
+    private NavMeshAgent m_Agent = null;
     private NavMeshSurface m_navmesh;
 
 
     private void Start()
     {
         //set new target location
-        UpdatePath(randomPath());
+        StartCoroutine(NavmeshLoad());
         m_Agent = GetComponent<NavMeshAgent>();
-        m_navmesh = mazeGen.GetComponent<NavMeshSurface>();
+
     }
 
     private void FixedUpdate()
@@ -41,7 +40,7 @@ public class AIMovement : MonoBehaviour
                     m_Agent.speed = ChaseSpeed;
                 }
                 //if current transform is euqal to targetlocation, set a new one
-                else if (transform.position == m_targetLocation)
+                else if (m_Agent.velocity == Vector3.zero)
                 {
                     UpdatePath(randomPath());
                 }
@@ -62,20 +61,21 @@ public class AIMovement : MonoBehaviour
 
     private void UpdatePath(Vector3 newLocation)
     {
-        m_CurrentPath = new NavMeshPath();
-        NavMesh.CalculatePath(transform.position, newLocation, NavMesh.AllAreas, m_CurrentPath);
+        m_targetLocation = newLocation;
+        Debug.Log("target: " + newLocation);
+        m_Agent.SetDestination(newLocation);
     }
 
     private Vector3 randomPath()
     {
         //get the array from mazegenerator
         maze = mazeGen.GetComponent<MazeGenerator>().mazeArray; //45 x 32
-        Vector3 tempPos = new Vector3(Random.Range(0, 45), 2.2f, Random.Range(0, 32)); //2.2f
+        Vector3 tempPos = new Vector3(Random.Range(0f, 32f) + 0.5f, 2.2f, Random.Range(0f, 45f) + 0.5f); //2.2f
 
         //pick a random location if it does equal zero, set to there, if not loop until it does.
         while (maze[(int)tempPos.x, (int)tempPos.z] ! == 1)
         {
-            tempPos = new Vector3(Random.Range(0, 45), 2.2f, Random.Range(0, 32));
+            tempPos = new Vector3(Random.Range(0, 32) + 0.5f, 2.2f, Random.Range(0, 45) + 0.5f);
             Debug.Log("loop");
         } 
         return tempPos;
@@ -85,6 +85,14 @@ public class AIMovement : MonoBehaviour
     {
         float tempDist = (player.transform.position - transform.position).magnitude;
         return tempDist;
+    }
+
+    IEnumerator NavmeshLoad()
+    {
+        yield return new WaitForSeconds(1f);
+        m_navmesh = mazeGen.GetComponent<NavMeshSurface>();
+
+        UpdatePath(randomPath());
     }
 }
 
