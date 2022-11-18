01c886a76bd6df3bf21f00b72f183a403eece1a2
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2022-11-18 13:54:45 +0000

    teleporters added

--- TryNotToDieInTheMaze/Assets/Scripts/Maze Scripts/Teleporter.cs
+++ TryNotToDieInTheMaze/Assets/Scripts/Maze Scripts/Teleporter.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     public bool m_Active = true;
-    public Teleporter m_Destination;
+    private Teleporter m_Destination;
+    private List<Teleporter> m_teleporters;
+    private int poslistLength;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (m_Active && other.tag == "Player")
+        if (m_Active && (other.tag == "Player" || other.tag == "Enemy"))
         {
+            PickRandomTeleporter();
             m_Destination.m_Active = false;
             other.transform.position = m_Destination.transform.position;
             Debug.Log("collided");
@@ -18,9 +21,28 @@ public class Teleporter : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" || other.tag == "Enemy")
         {
             m_Active = true;
+        }
+    }
+
+    private void PickRandomTeleporter()
+    {
+        int newpos = Random.Range(0, poslistLength);
+        m_Destination = m_teleporters[newpos];
+    }
+
+    public void SetTeleportersList(List<GameObject> teleporters, int listLength)
+    {
+        m_teleporters = new List<Teleporter>();
+        Debug.Log(teleporters);
+        poslistLength = listLength;
+
+        for (int i = 0; i < listLength; i++)
+        {
+            m_teleporters.Add(teleporters[i].GetComponent<Teleporter>());
+            Debug.Log(i);
         }
     }
 }
